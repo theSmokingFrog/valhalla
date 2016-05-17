aab49b909a2a89f40bd7e62a750d8f7240ed9ea3
andreibrasso <andrei.brasso@gmail.com>
2016-05-17 20:57:55 +0200

    Fix map area restriction

--- Viking.js
+++ Viking.js
@@ -46,8 +46,8 @@ Viking.prototype.getActionPosition = function () {
         position.x = this.position.x + p.x;
         position.y = this.position.y + p.y;
 
-        position.x = position.x < 0 ? 0 : position.x > mapSizeX ? mapSizeX : position.x;
-        position.y = position.y < 0 ? 0 : position.y > mapSizeY ? mapSizeY : position.y;
+        position.x = position.x < 0 ? 0 : position.x >= mapSizeX ? mapSizeX : position.x;
+        position.y = position.y < 0 ? 0 : position.y >= mapSizeY ? mapSizeY : position.y;
 
     } else {
         throw new Error(this.id + 'position of order is invalid');
